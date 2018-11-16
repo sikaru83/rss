af985698c1e68d83eae2c81dc26bb3712165496e
sikaru83 <sikaru83@gmail.com>
2018-11-17 08:13:34 +0900

    20181117-0813

--- commit.sh
+++ commit.sh
@@ -5,4 +5,3 @@ cd /volume1/homes/sikaru83/git/rss
 git add *
 git commit -m $NowDate
 git push
-flexget -c /var/services/homes/sikaru83/.flexget/config.yml -l /var/services/homes/sikaru83/log/flexget.log execute
